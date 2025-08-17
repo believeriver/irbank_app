3efc8b76be2ee7c7e95422465d6e66d3aa4f6338
believeriver <nobuyuki.galois@gmail.com>
2025-08-17 15:41:16 +0900

    20250817_03 update wsgi.py for gunicorn

--- views/flask/wsgi.py
+++ views/flask/wsgi.py
@@ -5,12 +5,13 @@ import logging
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from controllers.flask_webserver import server
-import config.settings
 
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 logger = logging.getLogger(__name__)
 
+application = server.app
+
 
 if __name__ == "__main__":
     logger.info("running server")
-    server.start(config.settings.DEBUG)
+    application.run(debug=False, threaded=True)
